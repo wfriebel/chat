6af2ecd3e6a725cf18033f74418a844088c307b6
Will Friebel <will.friebel@gmail.com>
2018-04-06 13:57:26 -0700

    add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 const socket = io();
+
+const messageInput = $('[name=message]');
+const locationButton = $('#send-location');
+
 socket.on('connect', () => {
   console.log('connected to server');
 })
@@ -15,7 +19,6 @@ socket.on('newMessage', (message) => {
 })
 
 $('#message-form').on('submit', function(e) {
-  const messageInput = $('[name=message]');
   e.preventDefault();
 
   const newMessage = {
@@ -28,21 +31,6 @@ $('#message-form').on('submit', function(e) {
   })
 })
 
-const locationButton = $('#send-location');
-locationButton.on('click', function(e) {
-  if(!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser')
-  }
-  navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit('createLocationMessage', {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    })
-  }, (e) => {
-    alert('Unable to fetch location');
-  })
-})
-
 socket.on('newLocationMessage', (locationMessage) => {
   const li = $('<li></li>');
   li.text(`${locationMessage.from}: `)
@@ -51,4 +39,24 @@ socket.on('newLocationMessage', (locationMessage) => {
   a.text(`My current location`);
   li.append(a);
   $('#messages').append(li);
+})
+
+locationButton.on('click', function(e) {
+  if(!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser')
+  }
+  locationButton.attr('disabled', 'disabled');
+  locationButton.text('Sending Location...');
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('createLocationMessage', {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    })
+    locationButton.text('Send Location');
+    locationButton.removeAttr('disabled');
+    messageInput.focus();
+  }, (e) => {
+    alert('Unable to fetch location');
+    locationButton.removeAttr('disabled');
+  })
 })
